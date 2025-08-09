9764e3399e8a89abf42b1901f2ef26ed9d6c71c2
Sikandar25 <59553904+Sikandar25@users.noreply.github.com>
2025-08-09 22:14:11 +0530

    Add files via upload

---
+++ Test_13_Cloud_Monitoring/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f072xb.h \
+ C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Include/core_cm0.h \
+ C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f072xb.h:
+C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Include/core_cm0.h:
+C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/HP/Desktop/C_Test\ Workspac/Stm_Test/en.stm32cubef0/STM32Cube_FW_F0_V1.11.0/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
